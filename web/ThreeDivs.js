a2ba81ee5deff1448376998cc7c8035329465daa
aslan602 <jos18002@byui.edu>
2020-01-15 13:04:41 -0700

    added Week 2 Team Assignment

---
+++ web/ThreeDivs.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+
+function bold(obj) {
+    obj.innerHTML = style.fontWeight = "bold";
+}
+
+function unbold(obj) {
+    obj.innerHTML = style.fontWeight = "normal";
+}
+
+

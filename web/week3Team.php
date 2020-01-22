ab96346520dd8a4de4ee3532a74f41233c7819b9
aslan602 <jos18002@byui.edu>
2020-01-21 17:38:25 -0700

    Team upload

---
+++ web/week3Team.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Week 3 Team PHP</title>
+</head>
+<body>
+    <div>
+	    Username:
+		<?php
+		$_POST["name"];
+		?>
+	</div>
+	<div>
+
+	</div>
+	<div>
+
+	</div>
+	<div>
+
+	</div>
+
+</body>
+</html>

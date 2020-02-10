278dfea5cc3f946d6212e8fa6eae00038a829314
aslan602 <jos18002@byui.edu>
2020-02-10 09:43:31 -0700

    Added SQL file and error correction to journal start

--- web/db/myDb.sql
+++ web/db/myDb.sql
@@ -4,14 +4,14 @@ heroku pg:psql
 
 CREATE TABLE names (
 	name_id 	SERIAL NOT NULL PRIMARY KEY,
-	username	VARCHAR(100) NOT NULL UNIQUE
+	username        VARCHAR(100) NOT NULL UNIQUE
 );
 
 CREATE TABLE journal (
-	journal_id	SERIAL NOT NULL PRIMARY KEY,
-	name_id		INT NOT NULL REFERENCES names(name_id),
-	entry		VARCHAR(10000) NOT NULL,
-	ts		TIMESTAMP
+	journal_id      SERIAL NOT NULL PRIMARY KEY,
+	name_id         INT NOT NULL REFERENCES names(name_id),
+	entry           VARCHAR(10000) NOT NULL,
+	ts              TIMESTAMP
 );
 
 

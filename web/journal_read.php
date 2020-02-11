5056c5a43a07ff9c23544eac8e0efa1f8583c4ee
aslan602 <jos18002@byui.edu>
2020-02-10 23:38:50 -0700

    Added new page for entry

--- web/journal_read.php
+++ web/journal_read.php
@@ -21,22 +21,25 @@
     <div class="fancy col-6 littlePad">Journal Entry:</div>
     <div class="col-6">
         <p class="center fancy pad">Hello <?php echo $_SESSION['username']; ?><br />Please select a journal entry to read<br /></p>
-    </div>
-    <div class="col-5 read">
-        <?php
+		 <?php
 		    require "dbConnect.php";
             $db = get_db();
 			$name = $_SESSION['username'];
             $statement = $db->prepare("SELECT name_id FROM names WHERE username = ?");
-            $nameid = $statement->execute([$name]);
-			echo "<p class='fancy'>Here is the Timestamps for " . $name . "and the name_id is " . $nameid . "</p><br /><br />";
+            $statement->execute([$name]);
+			&nameId = $statement->fetch(PDO::FETCH_ASSOC);
 			$statement = $db->prepare("SELECT ts FROM journal WHERE name_id = ?");
-            $times = $statement->execute(array([$nameid]));
-			foreach ($times as $time) {
-			   echo "<p class='button'>" . $time . "</p><br />";
-			}
+            $statement->execute([$nameId]);
+			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+               $timestamp = $row['ts'];
+               echo "<a href='https://powerful-basin-71330.herokuapp.com/journal_readentry.php?q=$timestamp' >" . $username . "</a><br />";
+            }
 			
         ?>
+    </div>
+    <div class="col-5 read">
+       
     </div>    
 </body>
 </html>
---
+++ web/journal_readentry.php
@@ -0,0 +1,36 @@
+﻿<?php
+   start_session();
+   $timestamp = $_REQUEST["q"];
+   $_SESSION["timestamp"] = $timestamp;
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Journal Past Entries</title>
+    <link rel="stylesheet" type="text/css" href="journal.css">
+    <script src="journal.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div class="heading col-6">
+        Journal
+    </div>
+    <div class="fancy col-6 littlePad">Journal Entry for Time Stamp " <?php echo $_SESSION['timestamp'] ?></div>
+    <div class="col-9 namelist">
+       <?php
+          $timestamp = $_SESSION["timestamp"];
+          require "dbConnect.php";
+          $db = get_db();
+          $statement = $db->prepare("SELECT entry FROM journal WHERE ts = ?");
+          $statement->execute([$timestamp]);
+		  &entry = $statement->fetch(PDO::FETCH_ASSOC);
+          echo "<p class='namelist'>" . $entry . "</p>";
+       ?>
+    </div>
+
+</body>
+</html>

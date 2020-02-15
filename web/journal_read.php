c8d099125c18d9477c3b67fe350268d57ab29a35
aslan602 <jos18002@byui.edu>
2020-02-14 22:16:37 -0700

    Fixing read five 5

--- web/journal_read.php
+++ web/journal_read.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <div class="heading col-8">
+    <div class="heading">
         Journal
     </div>	
     <div class="timelistpad">

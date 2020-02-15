dda57bb2e5a63545dda43151b187a748ab55ceea
aslan602 <jos18002@byui.edu>
2020-02-15 13:37:27 -0700

    Fixing write new name

--- web/writenewusername.php
+++ web/writenewusername.php
@@ -6,9 +6,13 @@
    	   die();
    }
    $_SESSION["username"] = $tempname;
+   
+   if ($_SESSION["username"] == null) { //session variable is set or die!
+   	   echo "ERROR GETTING NAME!";
+	   die();
+   }
    require "dbConnect.php";
-   $db = get_db();
-   $tname = "'" . $tempname . "'";
+   $db = get_db();   
    $statement = $db->prepare("INSERT INTO names (username) VALUES (:newname)");
    $statement->bindValue(":newname", $tempname, PDO::PARAM_STR);
    $statement->execute();

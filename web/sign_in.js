d1a5ad60a0e384897b8840a9b861c223041813c5
aslan602 <jos18002@byui.edu>
2020-02-22 09:50:58 -0700

    Adding error messages to sign in

--- web/sign_in.js
+++ web/sign_in.js
@@ -3,3 +3,8 @@
 function createaccount() {
     window.location.href = "create.php";
 }
+
+function incorrect() {
+    alert("Incorrect Password.  Try Again!");
+    window.location.href = "sign_in.php";
+}

efc33774183904627f0aa8c443fccf49069e3f98
aslan602 <jos18002@byui.edu>
2020-02-22 11:41:03 -0700

    Added new SQL file

--- web/db/myDb.sql
+++ web/db/myDb.sql
@@ -14,4 +14,12 @@ CREATE TABLE journal (
 	ts              TIMESTAMP
 );
 
+CREATE TABLE journalaccounts (
+	id SERIAL PRIMARY KEY NOT NULL,
+	username VARCHAR(50) UNIQUE NOT NULL,
+	password VARCHAR(80) NOT NULL
+);
+
+
+
 
